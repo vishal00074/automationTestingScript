0639b39890378eb5be18fcb775d361466de9ac14
vishal00074 <vc980531@gmail.com>
2025-02-19 19:15:46 +0530

    Added Fill form function

--- helpers/Fillform.php
+++ helpers/Fillform.php
@@ -8,8 +8,8 @@ class Fillform
     public $username_selector = "";
     public $password_selector = '';
 
-    public $submit_button_selector= "";
-    public $username= "";
+    public $submit_button_selector = "";
+    public $username = "";
     public $password = "";
 
     public function __construct()
@@ -18,16 +18,15 @@ class Fillform
     }
 
 
-    function fillForm()
+    public function fillForm()
     {
         $this->exts->capture("1-pre-login");
 
         $this->exts->log("Enter Username");
         $this->exts->moveToElementAndType($this->username_selector, $this->username);
-        sleep(2);
+
         $this->exts->log("Enter Password");
         $this->exts->moveToElementAndType($this->password_selector, $this->password);
-        sleep(2);
         $this->exts->capture("1-password-filled");
         $this->exts->moveToElementAndClick($this->submit_button_selector);
         sleep(6);
