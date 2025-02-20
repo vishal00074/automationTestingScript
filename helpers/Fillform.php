318a190f5c672fa29f99ae09b2b3e5190a467e4b
vishal00074 <vc980531@gmail.com>
2025-02-20 13:36:05 +0530

    added fillform

--- helpers/Fillform.php
+++ helpers/Fillform.php
@@ -11,6 +11,9 @@ class Fillform
     public $submit_button_selector = "";
     public $username = "";
     public $password = "";
+    public $loginUrl = "";
+    public $submit_login_selector = "";
+    
 
     public function __construct()
     {
@@ -30,5 +33,44 @@ class Fillform
         $this->exts->capture("1-password-filled");
         $this->exts->moveToElementAndClick($this->submit_button_selector);
         sleep(6);
+    }
+
+
+    private function checkFillLoginUndetected()
+    {
+        $this->exts->type_key_by_xdotool("Ctrl+t");
+        sleep(13);
+
+        $this->exts->type_key_by_xdotool("F5");
+
+        sleep(5);
+
+        $this->exts->type_text_by_xdotool($this->loginUrl);
+        $this->exts->type_key_by_xdotool("Return");
+        sleep(30);
+        for ($i = 0; $i < 5; $i++) {
+            $this->exts->type_key_by_xdotool("Tab");
+            sleep(1);
+        }
+        $this->exts->type_key_by_xdotool("Tab");
+        $this->exts->log("Enter Username");
+        $this->exts->type_text_by_xdotool($this->username);
+        $this->exts->capture("enter-username");
+        $this->exts->type_key_by_xdotool("Return");
+        sleep(5);
+        $this->exts->type_key_by_xdotool("Tab");
+        sleep(1);
+        $this->exts->log("Enter Password");
+        $this->exts->type_text_by_xdotool($this->password);
+        $this->exts->capture("enter-password");
+        sleep(5);
+        $this->exts->log("Submit Login Form");
+        $this->exts->moveToElementAndClick($this->submit_login_selector);
+        $this->exts->capture("submit-login");
+        sleep(5);
+        $this->exts->type_key_by_xdotool("Tab");
+        sleep(1);
+        $this->exts->type_key_by_xdotool("Return");
+        sleep(10);
     }
 }
