41604ef892c0aa3840d07a245b706ea286a69566
vishal00074 <vc980531@gmail.com>
2025-02-26 12:24:54 +0530

    Create js script for testing element in dom

---
+++ js/extractText.js
@@ -0,0 +1,6 @@
+/**
+ * The below code is for testing purpose on frontend
+ * how we can extract text by using querySelector and innerHTML text
+ *  
+ */
+document.body.innerHTML.includes("Your Error Message Here") // return true if your message is exists in dom
