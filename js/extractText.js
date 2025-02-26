00397e3d79ded6d407780799ae5470ca8789fb11
vishal00074 <vc980531@gmail.com>
2025-02-26 12:27:31 +0530

    Create js script for testing element in dom

--- js/extractText.js
+++ js/extractText.js
@@ -4,3 +4,18 @@
  *  
  */
 document.body.innerHTML.includes("Your Error Message Here") // return true if your message is exists in dom
+
+if (document.body.innerHTML.includes("Your Error Message Here")) {
+    console.log("Error message found in page source.");
+} else {
+    console.log("Error message not found.");
+}
+
+
+// This method find you message by using querySelector
+let errorMessage = document.querySelector("Your Dom Element"); //i.e span.loginErrorMessage
+if (errorMessage && errorMessage.innerText.trim() !== "") {
+    console.log("Element has text:", errorMessage.innerText);
+} else {
+    console.log("Element is empty or not found.");
+}
