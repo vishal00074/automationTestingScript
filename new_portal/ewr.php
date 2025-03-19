4d22cf339c4d1c23e49f7a4c19c5220d0d6c26a0
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-03-19 12:38:27 +0530

    added and updated script

---
+++ new_portal/ewr.php
@@ -0,0 +1,203 @@
+<?php
+
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package uwa
+ *
+ * @copyright   GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+
+$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
+require_once($gmi_browser_core);
+class PortalScriptCDP
+{
+
+    private $exts;
+    public $setupSuccess = false;
+    private $chrome_manage;
+    private $username;
+    private $password;
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->setupSuccess = true;
+    }
+
+    /*Define constants used in script*/
+    public $baseUrl = 'https://mein.ewr-internett.de/';
+    public $loginUrl = 'https://mein.ewr-internett.de/#{1}';
+    public $invoicePageUrl = '';
+
+    public $username_selector = 'input[id="ID_USERNAME"]';
+    public $password_selector = 'input[id="ID_PASSWORD"]';
+    public $remember_me_selector = '';
+    public $submit_login_selector = 'input[id="ID_LOGIN"]';
+
+    public $check_login_failed_selector = 'div.InfoBox_Error span[class*="BasicIcons-Error"]';
+    public $check_login_success_selector = 'a[id="ID_LogoutAction"]';
+
+    public $isNoInvoice = true;
+
+    /**
+     * Entry Method thats called for a portal
+     * @param Integer $count Number of times portal is retried.
+     */
+    private function initPortal($count)
+    {
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->openUrl($this->baseUrl);
+        $this->exts->loadCookiesFromFile();
+
+        if (!$this->checkLogin()) {
+            $this->exts->log('NOT logged via cookie');
+
+            $this->exts->clearCookies();
+            $this->exts->openUrl($this->loginUrl);
+            $this->fillForm(0);
+        }
+        if ($this->checkLogin()) {
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+            $this->exts->capture("LoginSuccess");
+            $elements = $this->exts->getElements('a[id*="ID_textactioncontrol"]');
+            try {
+                // Click on first element
+                $elements[0]->click();
+            } catch (\Exception $e) {
+                $this->exts->log(__FUNCTION__ . '::Invoice button Error ' . $e->getMessage());
+            }
+
+            $this->downloadInvoices();
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+
+            $this->exts->success();
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Use login failed');
+            $this->exts->log(__FUNCTION__ . '::Last URL: ' . $this->exts->getUrl());
+
+            $error_text = strtolower($this->exts->extract($this->check_login_failed_selector));
+            if (
+                stripos($error_text, 'ihre benutzername/passwortkombination ist nicht gültig.') !== false ||
+                stripos($error_text, 'ihre benutzername/passwortkombination ist nicht gültig. bitte versuchen sie es erneut.') !== false ||
+                stripos($error_text, 'your username/password combination is invalid.') !== false
+            ) {
+
+                $this->exts->loginFailure(1);
+            } else {
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    public function fillForm($count)
+    {
+        $this->exts->log("Begin fillForm " . $count);
+
+        $this->exts->waitTillPresent($this->username_selector);
+        if ($this->exts->querySelector($this->username_selector) != null) {
+
+            $this->exts->capture("1-pre-login");
+            $this->exts->log("Enter Username");
+            $this->exts->moveToElementAndType($this->username_selector, $this->username);
+            sleep(2);
+            $this->exts->log("Enter Password");
+            $this->exts->moveToElementAndType($this->password_selector, $this->password);
+            sleep(2);
+
+            if ($this->exts->exists($this->remember_me_selector)) {
+                $this->exts->click_by_xdotool($this->remember_me_selector);
+                sleep(2);
+            }
+
+            $this->exts->capture("1-login-page-filled");
+            if ($this->exts->exists($this->submit_login_selector)) {
+                $this->exts->moveToElementAndClick($this->submit_login_selector);
+                sleep(5);
+            }
+        }
+    }
+    /**
+     * Method to Check where user is logged in or not
+     * return boolean true/false
+     */
+    public  function checkLogin()
+    {
+        $this->exts->log("Begin checkLogin ");
+        $isLoggedIn = false;
+        try {
+            for ($wait = 0; $wait < 2 && $this->exts->executeSafeScript("return !!document.querySelector('" . $this->check_login_success_selector . "');") != 1; $wait++) {
+                $this->exts->log('Waiting for login.....');
+                sleep(10);
+            }
+            if ($this->exts->exists($this->check_login_success_selector)) {
+                $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+                $isLoggedIn = true;
+            }
+        } catch (Exception $exception) {
+
+            $this->exts->log("Exception checking loggedin " . $exception);
+        }
+        return $isLoggedIn;
+    }
+
+    private function downloadInvoices($count = 0)
+    {
+        $this->exts->log(__FUNCTION__);
+
+        $this->exts->waitTillPresent('table.Table_Standard tbody tr');
+        $this->exts->capture("4-invoices-classic");
+
+        $invoices = [];
+        $rows = $this->exts->getElements('table.Table_Standard tbody tr');
+        foreach ($rows as $key => $row) {
+            $invoiceLink = $this->exts->getElement('a[href*="/?qs_servlet=downloadIxServlet&"]:nth-child(2)', $row);
+            if ($invoiceLink != null) {
+                sleep(2);
+                $invoiceUrl = $invoiceLink->getAttribute("href");
+                $invoiceName = $this->exts->extract('td:nth-child(1)', $row);
+                $invoiceName =  $invoiceName ?? time();
+                $invoiceDate = $this->exts->extract('td:nth-child(2)', $row);
+                $invoiceAmount = '';
+
+                array_push($invoices, array(
+                    'invoiceName' => $invoiceName,
+                    'invoiceDate' => $invoiceDate,
+                    'invoiceAmount' => $invoiceAmount,
+                    'invoiceUrl' => $invoiceUrl,
+                ));
+                $this->isNoInvoice = false;
+            }
+        }
+
+        $this->exts->log('Invoices found: ' . count($invoices));
+        foreach ($invoices as $invoice) {
+            $this->exts->log('--------------------------');
+            $this->exts->log('invoiceName: ' . $invoice['invoiceName']);
+            $this->exts->log('invoiceDate: ' . $invoice['invoiceDate']);
+            $this->exts->log('invoiceAmount: ' . $invoice['invoiceAmount']);
+            $this->exts->log('invoiceUrl: ' . $invoice['invoiceUrl']);
+            $invoiceFileName = $invoice['invoiceName'] . '.pdf';
+            $invoice['invoiceDate'] = $this->exts->parse_date($invoice['invoiceDate'], 'd.m.Y', 'Y-m-d');
+            $this->exts->log('Date parsed: ' . $invoice['invoiceDate']);
+
+            $downloaded_file = $this->exts->direct_download($invoice['invoiceUrl'], 'pdf', $invoiceFileName);
+            if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                $this->exts->new_invoice($invoiceUrl, $invoice['invoiceDate'], $invoice['invoiceAmount'], $downloaded_file);
+                sleep(1);
+            } else {
+                $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+            }
+        }
+    }
+}
