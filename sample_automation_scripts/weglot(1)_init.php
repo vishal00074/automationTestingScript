8fb669797800c42e7dfda3778acc406e133d393a
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-03-21 12:45:05 +0530

    added and updated script

--- sample_automation_scripts/weglot(1)_init.php
+++ sample_automation_scripts/weglot(1)_init.php
@@ -8,7 +8,7 @@ public $login_tryout = 0;
 public $restrictPages = 3;
 public $totalFiles = 0;
 public $check_login_failed_selector = 'p[class="text-danger"]';
-public $check_login_success_selector = 'span[data-bs-popper="static"] a[href*="/logout"]';
+public $check_login_success_selector = 'a[href*="/logout"]';
 
 
 /**
@@ -21,53 +21,19 @@ private function initPortal($count)
 
     $this->restrictPages = isset($this->exts->config_array["restrictPages"]) ? (int)@$this->exts->config_array["restrictPages"] : 3;
 
-
-    $this->exts->openUrl($this->loginUrl);
+    $this->exts->openUrl($this->baseUrl);
     sleep(5);
     $this->exts->capture("Home-page-without-cookie");
-
-    $this->check_solve_blocked_page();
-
-    $isCookieLoginSuccess = false;
-    if ($this->exts->loadCookiesFromFile()) {
-
+    if (!$this->checkLogin()) {
+        $this->clearChrome();
         $this->exts->openUrl($this->loginUrl);
-        sleep(15);
-
-        if ($this->checkLogin()) {
-            $isCookieLoginSuccess = true;
-        } else {
-            $this->exts->clearCookies();
-            $this->exts->openUrl($this->loginUrl);
-            sleep(10);
-            $this->check_solve_blocked_page();
-        }
-    } else {
-
-        $this->exts->openUrl($this->loginUrl);
-        sleep(10);
-
-        $this->check_solve_blocked_page();
-    }
-
-    if (!$isCookieLoginSuccess) {
-        sleep(10);
-
-
+        sleep(2);
         $this->fillform(0);
-        sleep(10);
         $this->check_solve_blocked_page();
-        // div[class="row"] h2.h1
-        $this->exts->waitTillPresent('div[class="row"] h2.h1', 20);
-        if ($this->exts->exists('div[class="row"] h2.h1')) {
-            $this->exts->openUrl($this->loginUrl);
-            for ($i = 0; $i < 10; $i++) {
-                $this->exts->type_key_by_xdotool('Tab');
-                sleep(1);
-            }
-            $this->exts->type_key_by_xdotool('Return');
-            sleep(8);
-            $this->fillform(0);
+
+        // try again for form submission
+        if ($this->exts->exists($this->username_selector)) {
+            $this->fillform(1);
         }
     }
 
@@ -75,7 +41,7 @@ private function initPortal($count)
     if ($this->checkLogin()) {
         $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
         $this->exts->capture("LoginSuccess");
-
+        
         if (!empty($this->exts->config_array['allow_login_success_request'])) {
             $this->exts->triggerLoginSuccess();
         }
@@ -110,16 +76,15 @@ public function fillForm($count)
 
             $this->exts->log("Enter Password");
             $this->exts->moveToElementAndType($this->password_selector, $this->password);
-            sleep(5);
-
-            $this->checkFillRecaptcha();
+            sleep(20); // added wait before form submission 
 
             $this->exts->capture("login-fill-form");
 
             if ($this->exts->exists($this->submit_button_selector)) {
                 $this->exts->moveToElementAndClick($this->submit_button_selector);
+                sleep(10);
             }
-            sleep(10);
+
 
             // check login in failure case
             if (stripos(strtolower($this->exts->extract($this->check_login_failed_selector)), "Wrong credentials.") !== false) {
@@ -151,6 +116,42 @@ public function checkLogin()
     return $isLoggedIn;
 }
 
+/**
+    * Clears Chrome browser history, cookies, and cache.
+    * This method automates navigating to Chrome's "Clear Browsing Data" settings page 
+    * and selecting the necessary options using keyboard inputs.
+    */
+private function clearChrome()
+{
+
+    $this->exts->log("Clearing browser history, cookies, and cache");
+    $this->exts->openUrl('chrome://settings/clearBrowserData');
+    sleep(10);
+
+    $this->exts->capture("clear-page");
+    for ($i = 0; $i < 2; $i++) {
+        $this->exts->type_key_by_xdotool('Tab');
+        sleep(1);
+    }
+    $this->exts->type_key_by_xdotool('Tab');
+    sleep(1);
+    $this->exts->type_key_by_xdotool('Return');
+    sleep(1);
+
+    $this->exts->type_key_by_xdotool('a');
+    sleep(1);
+    $this->exts->type_key_by_xdotool('Return');
+    sleep(3);
+    $this->exts->capture("clear-page");
+    for ($i = 0; $i < 5; $i++) {
+        $this->exts->type_key_by_xdotool('Tab');
+        sleep(1);
+    }
+    $this->exts->type_key_by_xdotool('Return');
+    sleep(10);
+    $this->exts->capture("after-clear");
+}
+
 private function check_solve_blocked_page()
 {
     $this->exts->capture_by_chromedevtool("blocked-page-checking");
@@ -170,70 +171,5 @@ private function check_solve_blocked_page()
         } else {
             break;
         }
-    }
-}
-
-private function checkFillRecaptcha($count = 1)
-{
-    $this->exts->log(__FUNCTION__);
-    $recaptcha_iframe_selector = 'iframe[src*="google.com/recaptcha/api2/anchor"]';
-    $recaptcha_textarea_selector = 'textarea[name="g-recaptcha-response"]';
-    $this->exts->waitTillPresent($recaptcha_iframe_selector, 20);
-    if ($this->exts->exists($recaptcha_iframe_selector)) {
-        $iframeUrl = $this->exts->extract($recaptcha_iframe_selector, null, 'src');
-        $data_siteKey = explode('&', end(explode("&k=", $iframeUrl)))[0];
-        $this->exts->log("iframe url  - " . $iframeUrl);
-        $this->exts->log("SiteKey - " . $data_siteKey);
-
-        $isCaptchaSolved = $this->exts->processRecaptcha($this->exts->getUrl(), $data_siteKey, false);
-        $this->exts->log("isCaptchaSolved - " . $isCaptchaSolved);
-
-        if ($isCaptchaSolved) {
-            // Step 1 fill answer to textarea
-            $this->exts->log(__FUNCTION__ . "::filling reCaptcha response..");
-            $recaptcha_textareas =  $this->exts->querySelectorAll($recaptcha_textarea_selector);
-            for ($i = 0; $i < count($recaptcha_textareas); $i++) {
-                $this->exts->execute_javascript("arguments[0].innerHTML = '" . $this->exts->recaptcha_answer . "';", [$recaptcha_textareas[$i]]);
-            }
-            sleep(2);
-            $this->exts->capture('recaptcha-filled');
-
-            $gcallbackFunction = $this->exts->execute_javascript('
-                (function() { 
-                    if(document.querySelector("[data-callback]") != null){
-                        return document.querySelector("[data-callback]").getAttribute("data-callback");
-                    }
-
-                    var result = ""; var found = false;
-                    function recurse (cur, prop, deep) {
-                        if(deep > 5 || found){ return;}console.log(prop);
-                        try {
-                            if(cur == undefined || cur == null || cur instanceof Element || Object(cur) !== cur || Array.isArray(cur)){ return;}
-                            if(prop.indexOf(".callback") > -1){result = prop; found = true; return;
-                            } else { deep++;
-                                for (var p in cur) { recurse(cur[p], prop ? prop + "." + p : p, deep);}
-                            }
-                        } catch(ex) { console.log("ERROR in function: " + ex); return; }
-                    }
-
-                    recurse(___grecaptcha_cfg.clients[0], "", 0);
-                    return found ? "___grecaptcha_cfg.clients[0]." + result : null;
-                })();
-            ');
-            $this->exts->log('Callback function: ' . $gcallbackFunction);
-            $this->exts->log('Callback function: ' . $this->exts->recaptcha_answer);
-            if ($gcallbackFunction != null) {
-                $this->exts->execute_javascript($gcallbackFunction . '("' . $this->exts->recaptcha_answer . '");');
-                sleep(10);
-            }
-        } else {
-            // try again if recaptcha expired
-            if ($count < 3) {
-                $count++;
-                $this->checkFillRecaptcha($count);
-            }
-        }
-    } else {
-        $this->exts->log(__FUNCTION__ . '::Not found reCaptcha');
     }
 }
