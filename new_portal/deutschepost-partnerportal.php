e2193a10073867dd76d8b919eca3d90ee921668d
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-03-25 09:57:02 +0530

    updated script

---
+++ new_portal/deutschepost-partnerportal.php
@@ -0,0 +1,185 @@
+<?php
+
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package uwa
+ *
+ * @copyright   GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+
+$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
+require_once($gmi_browser_core);
+class PortalScriptCDP
+{
+
+    private $exts;
+    public $setupSuccess = false;
+    private $chrome_manage;
+    private $username;
+    private $password;
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->setupSuccess = true;
+    }
+
+    /*Define constants used in script*/
+    public $baseUrl = 'https://internet.partnerportal-deutschepost.de/';
+    public $loginUrl = 'https://internet.partnerportal-deutschepost.de/login';
+    public $invoicePageUrl = 'https://internet.partnerportal-deutschepost.de/meine-filiale/zahlen-daten-fakten/verguetungsabrechnung';
+
+    public $username_selector = 'input[id="loginUser"]';
+    public $password_selector = 'input[id="loginPlaintextPassword"]';
+    public $remember_me_selector = '';
+    public $submit_login_selector = 'input[class="standard-button"]';
+
+    public $check_login_failed_selector = 'div.tx-felogin-pi1 > font';
+    public $check_login_success_selector = 'div#logout';
+
+    public $isNoInvoice = true;
+
+    /**
+     * Entry Method thats called for a portal
+     * @param Integer $count Number of times portal is retried.
+     */
+    private function initPortal($count)
+    {
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->openUrl($this->baseUrl);
+        $this->exts->loadCookiesFromFile();
+
+        if (!$this->checkLogin()) {
+            $this->exts->log('NOT logged via cookie');
+
+            $this->exts->clearCookies();
+            $this->exts->openUrl($this->loginUrl);
+            $this->fillForm(0);
+        }
+        if ($this->checkLogin()) {
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+            $this->exts->capture("LoginSuccess");
+
+            $this->exts->openUrl($this->invoicePageUrl);
+            $this->downloadInvoices();
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+
+            $this->exts->success();
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Use login failed');
+            $this->exts->log(__FUNCTION__ . '::Last URL: ' . $this->exts->getUrl());
+
+            $error_text = strtolower($this->exts->extract($this->check_login_failed_selector));
+            if (
+                stripos($error_text, 'log in here with your personal access to the partner portal.') !== false ||
+                stripos($error_text, 'melden Sie sich hier mit Ihrem persönlichen zugang zum partnerportal an.') !== false
+            ) {
+                $this->exts->loginFailure(1);
+            } else {
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    public function fillForm($count)
+    {
+        $this->exts->log("Begin fillForm " . $count);
+
+        $this->exts->waitTillPresent($this->username_selector);
+        if ($this->exts->querySelector($this->username_selector) != null) {
+
+            $this->exts->capture("1-pre-login");
+            $this->exts->log("Enter Username");
+            $this->exts->moveToElementAndType($this->username_selector, $this->username);
+            sleep(2);
+            $this->exts->log("Enter Password");
+            $this->exts->moveToElementAndType($this->password_selector, $this->password);
+            sleep(2);
+
+            if ($this->exts->exists($this->remember_me_selector)) {
+                $this->exts->click_by_xdotool($this->remember_me_selector);
+                sleep(2);
+            }
+
+            $this->exts->capture("1-login-page-filled");
+            if ($this->exts->exists($this->submit_login_selector)) {
+                $this->exts->moveToElementAndClick($this->submit_login_selector);
+                sleep(5);
+            }
+        }
+    }
+    /**
+     * Method to Check where user is logged in or not
+     * return boolean true/false
+     */
+    public  function checkLogin()
+    {
+        $this->exts->log("Begin checkLogin ");
+        $isLoggedIn = false;
+        try {
+            for ($wait = 0; $wait < 2 && $this->exts->executeSafeScript("return !!document.querySelector('" . $this->check_login_success_selector . "');") != 1; $wait++) {
+                $this->exts->log('Waiting for login.....');
+                sleep(10);
+            }
+            if ($this->exts->exists($this->check_login_success_selector)) {
+                $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+                $isLoggedIn = true;
+            }
+        } catch (Exception $exception) {
+
+            $this->exts->log("Exception checking loggedin " . $exception);
+        }
+        return $isLoggedIn;
+    }
+
+    private function downloadInvoices($count = 0)
+    {
+        $this->exts->log(__FUNCTION__);
+
+        $this->exts->waitTillPresent('table tbody tr');
+        $this->exts->capture("4-invoices-classic");
+
+        $rows = $this->exts->getElements('table tbody tr');
+        foreach ($rows as $key => $row) {
+            $invoiceBtn = $this->exts->getElement('a', $row);
+            if ($invoiceBtn != null) {
+                sleep(2);
+                $invoiceUrl = $invoiceBtn->getAttribute("href");
+                $invoiceName =   time();
+                $invoiceDate = $this->exts->extract('td:nth-child(4)', $row);
+                $invoiceAmount = '';
+
+                $this->isNoInvoice = false;
+
+                $this->exts->log('--------------------------');
+                $this->exts->log('invoiceName: ' .  $invoiceName);
+                $this->exts->log('invoiceDate: ' . $invoiceDate);
+                $this->exts->log('invoiceAmount: ' . $invoiceAmount);
+                $this->exts->log('invoiceUrl: ' .  $invoiceUrl);
+                $invoiceFileName =  $invoiceName . '.pdf';
+                $invoiceDate = $this->exts->parse_date($invoiceDate, 'd.m.Y', 'Y-m-d');
+                $this->exts->log('Date parsed: ' .  $invoiceDate);
+
+                $downloaded_file = $this->exts->click_and_download($invoiceBtn, 'pdf', $invoiceFileName);
+                sleep(2);
+                if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                    $this->exts->new_invoice($invoiceUrl,  $invoiceDate, $invoiceAmount, $downloaded_file);
+                    sleep(1);
+                } else {
+                    $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+                }
+            }
+        }
+    }
+}
