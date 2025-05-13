91a522ffcde26dc80cbcf9140c76c72d95e6cddf
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-05-13 17:29:00 +0530

    updated script

--- sample_automation_scripts/adobe.php
+++ sample_automation_scripts/adobe.php
@@ -175,21 +175,21 @@ $this->exts->account_not_ready();
         $this->exts->openUrl('chrome://extensions/?id=cjpalhdlnbpafiamejdnhcphjbkeiagm'); // disable Block origin extension
         sleep(2);
         $this->exts->executeSafeScript("
-if(document.querySelector('extensions-manager') != null) {
-if(document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view')  != null){
-    var disable_button = document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]');
-    if(disable_button != null){
-        disable_button.click();
-    }
-}
-}
-");
-        sleep(1);
-        $this->exts->openUrl('chrome://extensions/?id=ifibfemgeogfhoebkmokieepdoobkbpo');
-        sleep(1);
-        $this->exts->executeSafeScript("if (document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]') != null) {
-document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]').click();
-}");
+            if(document.querySelector('extensions-manager') != null) {
+            if(document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view')  != null){
+                var disable_button = document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]');
+                if(disable_button != null){
+                    disable_button.click();
+                }
+            }
+            }
+            ");
+                    sleep(1);
+                    $this->exts->openUrl('chrome://extensions/?id=ifibfemgeogfhoebkmokieepdoobkbpo');
+                    sleep(1);
+                    $this->exts->executeSafeScript("if (document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]') != null) {
+            document.querySelector('extensions-manager').shadowRoot.querySelector('extensions-detail-view').shadowRoot.querySelector('#enableToggle[checked]').click();
+            }");
         sleep(2);
     }
 
