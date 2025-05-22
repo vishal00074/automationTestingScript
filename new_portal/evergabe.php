5502d200e85b68ab03b1957c121120e374c23843
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-05-22 18:24:33 +0530

    created script

---
+++ new_portal/evergabe.php
@@ -0,0 +1,239 @@
+<?php
+
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package uwa
+ *
+ * @copyright   GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+
+$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
+require_once($gmi_browser_core);
+class PortalScriptCDP
+{
+
+    private $exts;
+    public $setupSuccess = false;
+    private $chrome_manage;
+    private $username;
+    private $password;
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->setupSuccess = true;
+    }
+
+    /*Define constants used in script*/
+    public $baseUrl = 'https://www.evergabe.de/mein-evergabe/bestellungen';
+    public $loginUrl = 'https://www.evergabe.de/mein-evergabe/bestellungen';
+    public $invoicePageUrl = 'https://www.evergabe.de/mein-evergabe/bestellungen';
+
+    public $username_selector = 'input[name="username"]';
+    public $password_selector = 'input[name="password"]';
+    public $remember_me_selector = '';
+    public $submit_login_selector = 'input[type="submit"]';
+
+    public $check_login_failed_selector = 'span#input-error';
+    public $check_login_success_selector = 'a[href="/mein-evergabe/dashboard"]';
+
+    public $isNoInvoice = true;
+
+    /**
+     * Entry Method thats called for a portal
+     * @param Integer $count Number of times portal is retried.
+     */
+    private function initPortal($count)
+    {
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->openUrl($this->baseUrl);
+        sleep(10);
+        $this->exts->loadCookiesFromFile();
+
+        if ($this->exts->exists('div[id="usercentrics-root"]')) {
+            $this->switchToFrame('div[id="usercentrics-root"]');
+            sleep(2);
+
+            if ($this->exts->exists('button[data-testid="uc-accept-all-button"]')) {
+                $this->exts->moveToElementAndClick('button[data-testid="uc-accept-all-button"]');
+                sleep(2);
+            }
+            $this->exts->openUrl($this->baseUrl);
+            sleep(10);
+        }
+
+        if (!$this->checkLogin()) {
+            $this->exts->log('NOT logged via cookie');
+
+            $this->exts->clearCookies();
+            $this->exts->openUrl($this->loginUrl);
+            $this->fillForm(0);
+            sleep(15);
+
+            if ($this->exts->exists('button[name="resignin"]')) {
+                $this->exts->click_by_xdotool('button[name="resignin"]');
+                sleep(2);
+            }
+            $this->exts->openUrl($this->invoicePageUrl);
+            sleep(10);
+        }
+        if ($this->checkLogin()) {
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+            $this->exts->capture("LoginSuccess");
+
+            $this->exts->openUrl($this->invoicePageUrl);
+            $this->downloadInvoices();
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+
+            $this->exts->success();
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Use login failed');
+            $this->exts->log(__FUNCTION__ . '::Last URL: ' . $this->exts->getUrl());
+
+            $error_text = strtolower($this->exts->extract($this->check_login_failed_selector));
+
+            $this->exts->log(__FUNCTION__ . '::Error text: ' . $error_text);
+            if (stripos($error_text, strtolower('Ungültiger Benutzername oder Passwort')) !== false) {
+                $this->exts->loginFailure(1);
+            } else {
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    public function switchToFrame($query_string)
+    {
+        $this->exts->log(__FUNCTION__ . " Begin with " . $query_string);
+        $frame = null;
+        if (is_string($query_string)) {
+            $frame = $this->exts->queryElement($query_string);
+        }
+
+        if ($frame != null) {
+            $frame_context = $this->exts->get_frame_excutable_context($frame);
+            if ($frame_context != null) {
+                $this->exts->current_context = $frame_context;
+                return true;
+            }
+        } else {
+            $this->exts->log(__FUNCTION__ . " Frame not found " . $query_string);
+        }
+
+        return false;
+    }
+
+    public function fillForm($count)
+    {
+        $this->exts->log("Begin fillForm " . $count);
+
+        $this->exts->waitTillPresent($this->username_selector);
+        if ($this->exts->querySelector($this->username_selector) != null) {
+
+            $this->exts->capture("1-pre-login");
+            $this->exts->log("Enter Username");
+            $this->exts->moveToElementAndType($this->username_selector, $this->username);
+            sleep(2);
+            $this->exts->log("Enter Password");
+            $this->exts->moveToElementAndType($this->password_selector, $this->password);
+            sleep(2);
+
+            if ($this->exts->exists($this->remember_me_selector)) {
+                $this->exts->click_by_xdotool($this->remember_me_selector);
+                sleep(2);
+            }
+
+            $this->exts->capture("1-login-page-filled");
+            if ($this->exts->exists($this->submit_login_selector)) {
+                $this->exts->moveToElementAndClick($this->submit_login_selector);
+                sleep(2);
+            }
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Login page not found');
+            $this->exts->capture("2-login-page-not-found");
+        }
+    }
+    /**
+     * Method to Check where user is logged in or not
+     * return boolean true/false
+     */
+    public  function checkLogin()
+    {
+        $this->exts->log("Begin checkLogin ");
+        $isLoggedIn = false;
+        try {
+            for ($wait = 0; $wait < 2 && $this->exts->executeSafeScript("return !!document.querySelector('" . $this->check_login_success_selector . "');") != 1; $wait++) {
+                $this->exts->log('Waiting for login.....');
+                sleep(10);
+            }
+            if ($this->exts->exists($this->check_login_success_selector)) {
+                $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+                $isLoggedIn = true;
+            }
+        } catch (Exception $exception) {
+
+            $this->exts->log("Exception checking loggedin " . $exception);
+        }
+        return $isLoggedIn;
+    }
+
+    private function downloadInvoices($count = 1)
+    {
+        $this->exts->log(__FUNCTION__);
+
+        $this->exts->waitTillPresent('div#order ul li');
+        $this->exts->capture("4-invoices-classic");
+
+        $invoices = [];
+        $rows = $this->exts->getElements('div#order ul li');
+        foreach ($rows as $key => $row) {
+            $invoiceLink = $this->exts->getElement('a', $row);
+            if ($invoiceLink != null) {
+                $invoiceUrl = $invoiceLink->getAttribute("href");
+                $parts = explode('/', $invoiceUrl);
+                $invoiceName = $parts[5];
+                $invoiceDate = $this->exts->extract('span.link-text', $row);
+                $invoiceAmount = $this->exts->extract('div.result-list-item-cell:nth-child(5) > font', $row);
+
+                array_push($invoices, array(
+                    'invoiceName' => $invoiceName,
+                    'invoiceDate' => $invoiceDate,
+                    'invoiceAmount' => $invoiceAmount,
+                    'invoiceUrl' => $invoiceUrl,
+                ));
+                $this->isNoInvoice = false;
+            }
+        }
+
+        $this->exts->log('Invoices found: ' . count($invoices));
+        foreach ($invoices as $invoice) {
+            $this->exts->log('--------------------------');
+            $this->exts->log('invoiceName: ' . $invoice['invoiceName']);
+            $this->exts->log('invoiceDate: ' . $invoice['invoiceDate']);
+            $this->exts->log('invoiceAmount: ' . $invoice['invoiceAmount']);
+            $this->exts->log('invoiceUrl: ' . $invoice['invoiceUrl']);
+
+            $invoiceFileName = !empty($invoice['invoiceName']) ?  $invoice['invoiceName'] . '.pdf' : '';
+            $invoice['invoiceDate'] = $this->exts->parse_date($invoice['invoiceDate'], 'd.m.Y', 'Y-m-d');
+            $this->exts->log('Date parsed: ' . $invoice['invoiceDate']);
+
+            $downloaded_file = $this->exts->direct_download($invoice['invoiceUrl'], 'pdf', $invoiceFileName);
+            if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                $this->exts->new_invoice($invoice['invoiceName'], $invoice['invoiceDate'], $invoice['invoiceAmount'], $invoiceFileName);
+                sleep(1);
+            } else {
+                $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+            }
+        }
+    }
+}
