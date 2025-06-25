7c488e609d68b26f1f4fe9e4d6f1241af54e9a7e
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-06-25 18:21:47 +0530

    updated webgo script

---
+++ sample_automation_scripts/webgo.php
@@ -0,0 +1,260 @@
+<?php // I have added twofa code and updated download code
+
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package uwa
+ *
+ * @copyright   GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+
+$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
+require_once($gmi_browser_core);
+class PortalScriptCDP
+{
+
+    private $exts;
+    public $setupSuccess = false;
+    private $chrome_manage;
+    private $username;
+    private $password;
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->setupSuccess = true;
+    }
+
+    /**
+     * Method that called first for executing portal script, this method should not be altered by Users.
+     */
+    public function run()
+    {
+        if ($this->setupSuccess) {
+            try {
+                // Start portal script execution
+                $this->initPortal(0);
+            } catch (\Exception $exception) {
+                $this->exts->log('Exception: ' . $exception->getMessage());
+                $this->exts->capture("error");
+                var_dump($exception);
+            }
+
+
+            $this->exts->log('Execution completed');
+
+            $this->exts->process_completed();
+            $this->exts->dump_session_files();
+        } else {
+            echo 'Script execution failed.. ' . "\n";
+        }
+    }
+
+    // Server-Portal-ID: 7451 - Last modified: 27.11.2024 13:18:22 UTC - User: 1
+
+    public $baseUrl = 'https://login.webgo.de/Bills';
+    public $loginUrl = 'https://login.webgo.de/login';
+    public $invoicePageUrl = 'https://login.webgo.de/Bills';
+
+    public $username_selector = 'input#username';
+    public $password_selector = 'input#password';
+    public $remember_me_selector = '';
+    public $submit_login_selector = 'button[type="submit"], input[type="submit"]';
+
+    public $check_login_failed_selector = 'div.loginformerror, #input-error';
+    public $check_login_success_selector = 'a[href="/logout"], button[name="logout"]';
+
+    public $isNoInvoice = true;
+    /**
+     * Entry Method thats called for a portal
+     * @param Integer $count Number of times portal is retried.
+     */
+    private function initPortal($count)
+    {
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->openUrl($this->baseUrl);
+        sleep(1);
+
+        // Load cookies
+        $this->exts->loadCookiesFromFile();
+        sleep(1);
+        $this->exts->openUrl($this->baseUrl);
+        sleep(10);
+        $this->exts->capture('1-init-page');
+
+        // If user hase not logged in from cookie, clear cookie, open the login url and do login
+        if ($this->exts->getElement($this->check_login_success_selector) == null) {
+            $this->exts->log('NOT logged via cookie');
+            $this->exts->clearCookies();
+            $this->exts->openUrl($this->loginUrl);
+            sleep(15);
+            $this->checkFillLogin();
+            sleep(15);
+
+            $this->checkFillTwoFactor();
+        }
+
+
+        if ($this->exts->getElement($this->check_login_success_selector) != null) {
+            sleep(3);
+            $this->exts->log(__FUNCTION__ . '::User logged in');
+            $this->exts->capture("3-login-success");
+
+            // Open invoices url and download invoice
+            // $this->exts->openUrl($this->invoicePageUrl);
+            $this->exts->moveToElementAndClick('a[href*="/finance/invoices/"]');
+            $this->processInvoices();
+
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+            $this->exts->success();
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Use login failed');
+            if (
+                stripos($this->exts->extract($this->check_login_failed_selector), 'oder Passwort') !== false ||
+                stripos($this->exts->extract('span[id="input-error-otp-code"]'), 'One-time') !== false
+            ) {
+                $this->exts->loginFailure(1);
+            } else {
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    private function checkFillLogin()
+    {
+        if ($this->exts->getElement($this->password_selector) != null) {
+            sleep(3);
+            $this->exts->capture("2-login-page");
+
+            $this->exts->log("Enter Username");
+            $this->exts->moveToElementAndType($this->username_selector, $this->username);
+            sleep(1);
+
+            $this->exts->log("Enter Password");
+            $this->exts->moveToElementAndType($this->password_selector, $this->password);
+            sleep(1);
+
+            if ($this->remember_me_selector != '')
+                $this->exts->moveToElementAndClick($this->remember_me_selector);
+            sleep(2);
+
+            $this->exts->capture("2-login-page-filled");
+            $this->exts->moveToElementAndClick($this->submit_login_selector);
+        } else {
+            $this->exts->log(__FUNCTION__ . '::Login page not found');
+            $this->exts->capture("2-login-page-not-found");
+        }
+    }
+
+    private function checkFillTwoFactor()
+    {
+        $two_factor_selector = 'input[id="otp"]';
+        $two_factor_message_selector = '';
+        $two_factor_submit_selector = 'input[id="kc-login"]';
+
+        if ($this->exts->querySelector($two_factor_selector) != null && $this->exts->two_factor_attempts < 3) {
+            $this->exts->log("Two factor page found.");
+            $this->exts->capture("2.1-two-factor");
+
+            if ($this->exts->querySelector($two_factor_message_selector) != null) {
+                $this->exts->two_factor_notif_msg_en = "";
+                for ($i = 0; $i < count($this->exts->getElements($two_factor_message_selector)); $i++) {
+                    $this->exts->two_factor_notif_msg_en = $this->exts->two_factor_notif_msg_en . $this->exts->getElements($two_factor_message_selector)[$i]->getAttribute('innerText') . "\n";
+                }
+                $this->exts->two_factor_notif_msg_en = trim($this->exts->two_factor_notif_msg_en);
+                $this->exts->two_factor_notif_msg_de = $this->exts->two_factor_notif_msg_en;
+                $this->exts->log("Message:\n" . $this->exts->two_factor_notif_msg_en);
+            }
+            if ($this->exts->two_factor_attempts == 2) {
+                $this->exts->two_factor_notif_msg_en = $this->exts->two_factor_notif_msg_en . ' ' . $this->exts->two_factor_notif_msg_retry_en;
+                $this->exts->two_factor_notif_msg_de = $this->exts->two_factor_notif_msg_de . ' ' . $this->exts->two_factor_notif_msg_retry_de;
+            }
+
+            $two_factor_code = trim($this->exts->fetchTwoFactorCode());
+
+            if (!empty($two_factor_code) && trim($two_factor_code) != '') {
+                $this->exts->log("checkFillTwoFactor: Entering two_factor_code." . $two_factor_code);
+                $this->exts->moveToElementAndType($two_factor_selector, $two_factor_code);
+
+                $this->exts->log("checkFillTwoFactor: Clicking submit button.");
+                sleep(3);
+                $this->exts->capture("2.2-two-factor-filled-" . $this->exts->two_factor_attempts);
+
+                $this->exts->moveToElementAndClick($two_factor_submit_selector);
+                sleep(10);
+                if ($this->exts->exists('div[class*="errorMessage"]')) {
+
+                    $this->exts->capture("wrong 2FA code error-" . $this->exts->two_factor_attempts);
+                    $this->exts->log('The code you entered is incorrect. Please try again.');
+                }
+
+                if ($this->exts->querySelector($two_factor_selector) == null) {
+                    $this->exts->log("Two factor solved");
+                } else if ($this->exts->two_factor_attempts < 3) {
+                    $this->exts->notification_uid = '';
+                    $this->exts->two_factor_attempts++;
+                    $this->checkFillTwoFactor();
+                } else {
+                    $this->exts->log("Two factor can not solved");
+                }
+            } else {
+                $this->exts->log("Not received two factor code");
+            }
+        }
+    }
+
+    private function processInvoices()
+    {
+        sleep(25);
+        $this->exts->capture("4-invoices-page");
+        $invoices = [];
+
+        $rows = $this->exts->getElements('table > tbody > tr');
+        for ($i  = 0; $i < count($rows); $i++) {
+            $row = $this->exts->getElements('table > tbody > tr')[$i];
+            $tags = $this->exts->getElements('td', $row);
+            if (count($tags) >= 5 && $this->exts->getElement('button[name*="downloadInvoice"]', $tags[5]) != null) {
+                $invoiceButton = $this->exts->getElement('button[name*="downloadInvoice"]', $tags[5]);
+                $invoiceName = trim($tags[0]->getAttribute('innerText'));
+                $invoiceDate = trim($tags[2]->getAttribute('innerText'));
+                $invoiceAmount = trim(preg_replace('/[^\d\.\,]/', '', $tags[1]->getAttribute('innerText'))) . ' EUR';
+
+                $this->exts->log('--------------------------');
+                $this->exts->log('invoiceName: ' . $invoiceName);
+                $this->exts->log('invoiceDate: ' . $invoiceDate);
+                $this->exts->log('invoiceAmount: ' . $invoiceAmount);
+                // $this->exts->log('invoiceButton: '.$invoiceButton);
+
+                $invoiceFileName = !empty($invoiceName) ? $invoiceName . '.pdf' : '';
+                $invoiceDate = $this->exts->parse_date($invoiceDate, 'd.m.Y', 'Y-m-d');
+                $this->exts->log('Date parsed: ' . $invoiceDate);
+                $this->isNoInvoice = false;
+
+                $downloaded_file = $this->exts->click_and_download($invoiceButton, 'pdf', $invoiceFileName);
+                if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                    $this->exts->new_invoice($invoiceName, $invoiceDate, $invoiceAmount, $invoiceFileName);
+                    sleep(1);
+                } else {
+                    $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+                }
+            }
+        }
+    }
+}
+
+exec("docker rm -f selenium-node-111");
+exec("docker run -d --shm-size 2g -p 5902:5900 -p 9990:9999 -e TZ=Europe/Berlin -e SE_NODE_SESSION_TIMEOUT=86400 -e LANG=de -e GRID_TIMEOUT=0 -e GRID_BROWSER_TIMEOUT=0 -e SCREEN_WIDTH=1920 -e SCREEN_HEIGHT=1080 --name selenium-node-111 -v /var/www/remote-chrome/downloads:/home/seluser/Downloads/111 remote-chrome:v1");
+
+$browserSelected = 'chrome';
+$portal = new PortalScriptCDP($browserSelected, 'test_remote_chrome', '111', 'office@sayaq-adventures-muenchen.com', 'Sayaq#2022');
+$portal->run();
---
+++ sample_automation_scripts/webgo_init.php
@@ -0,0 +1,148 @@
+public $baseUrl = 'https://login.webgo.de/Bills';
+public $loginUrl = 'https://login.webgo.de/login';
+public $invoicePageUrl = 'https://login.webgo.de/Bills';
+
+public $username_selector = 'input#username';
+public $password_selector = 'input#password';
+public $remember_me_selector = '';
+public $submit_login_selector = 'button[type="submit"], input[type="submit"]';
+
+public $check_login_failed_selector = 'div.loginformerror, #input-error';
+public $check_login_success_selector = 'a[href="/logout"], button[name="logout"]';
+
+public $isNoInvoice = true;
+/**
+    * Entry Method thats called for a portal
+    * @param Integer $count Number of times portal is retried.
+    */
+private function initPortal($count)
+{
+    $this->exts->log('Begin initPortal ' . $count);
+    $this->exts->openUrl($this->baseUrl);
+    sleep(1);
+
+    // Load cookies
+    $this->exts->loadCookiesFromFile();
+    sleep(1);
+    $this->exts->openUrl($this->baseUrl);
+    sleep(10);
+    $this->exts->capture('1-init-page');
+
+    // If user hase not logged in from cookie, clear cookie, open the login url and do login
+    if ($this->exts->getElement($this->check_login_success_selector) == null) {
+        $this->exts->log('NOT logged via cookie');
+        $this->exts->clearCookies();
+        $this->exts->openUrl($this->loginUrl);
+        sleep(15);
+        $this->checkFillLogin();
+        sleep(15);
+
+        $this->checkFillTwoFactor();
+    }
+
+
+    if ($this->exts->getElement($this->check_login_success_selector) != null) {
+        sleep(3);
+        $this->exts->log(__FUNCTION__ . '::User logged in');
+        $this->exts->capture("3-login-success");
+
+        if (!empty($this->exts->config_array['allow_login_success_request'])) {
+            $this->exts->triggerLoginSuccess();
+        }
+
+        $this->exts->success();
+    } else {
+        $this->exts->log(__FUNCTION__ . '::Use login failed');
+        if (
+            stripos($this->exts->extract($this->check_login_failed_selector), 'oder Passwort') !== false ||
+            stripos($this->exts->extract('span[id="input-error-otp-code"]'), 'One-time') !== false
+        ) {
+            $this->exts->loginFailure(1);
+        } else {
+            $this->exts->loginFailure();
+        }
+    }
+}
+
+private function checkFillLogin()
+{
+    if ($this->exts->getElement($this->password_selector) != null) {
+        sleep(3);
+        $this->exts->capture("2-login-page");
+
+        $this->exts->log("Enter Username");
+        $this->exts->moveToElementAndType($this->username_selector, $this->username);
+        sleep(1);
+
+        $this->exts->log("Enter Password");
+        $this->exts->moveToElementAndType($this->password_selector, $this->password);
+        sleep(1);
+
+        if ($this->remember_me_selector != '')
+            $this->exts->moveToElementAndClick($this->remember_me_selector);
+        sleep(2);
+
+        $this->exts->capture("2-login-page-filled");
+        $this->exts->moveToElementAndClick($this->submit_login_selector);
+    } else {
+        $this->exts->log(__FUNCTION__ . '::Login page not found');
+        $this->exts->capture("2-login-page-not-found");
+    }
+}
+
+private function checkFillTwoFactor()
+{
+    $two_factor_selector = 'input[id="otp"]';
+    $two_factor_message_selector = '';
+    $two_factor_submit_selector = 'input[id="kc-login"]';
+
+    if ($this->exts->querySelector($two_factor_selector) != null && $this->exts->two_factor_attempts < 3) {
+        $this->exts->log("Two factor page found.");
+        $this->exts->capture("2.1-two-factor");
+
+        if ($this->exts->querySelector($two_factor_message_selector) != null) {
+            $this->exts->two_factor_notif_msg_en = "";
+            for ($i = 0; $i < count($this->exts->getElements($two_factor_message_selector)); $i++) {
+                $this->exts->two_factor_notif_msg_en = $this->exts->two_factor_notif_msg_en . $this->exts->getElements($two_factor_message_selector)[$i]->getAttribute('innerText') . "\n";
+            }
+            $this->exts->two_factor_notif_msg_en = trim($this->exts->two_factor_notif_msg_en);
+            $this->exts->two_factor_notif_msg_de = $this->exts->two_factor_notif_msg_en;
+            $this->exts->log("Message:\n" . $this->exts->two_factor_notif_msg_en);
+        }
+        if ($this->exts->two_factor_attempts == 2) {
+            $this->exts->two_factor_notif_msg_en = $this->exts->two_factor_notif_msg_en . ' ' . $this->exts->two_factor_notif_msg_retry_en;
+            $this->exts->two_factor_notif_msg_de = $this->exts->two_factor_notif_msg_de . ' ' . $this->exts->two_factor_notif_msg_retry_de;
+        }
+
+        $two_factor_code = trim($this->exts->fetchTwoFactorCode());
+
+        if (!empty($two_factor_code) && trim($two_factor_code) != '') {
+            $this->exts->log("checkFillTwoFactor: Entering two_factor_code." . $two_factor_code);
+            $this->exts->moveToElementAndType($two_factor_selector, $two_factor_code);
+
+            $this->exts->log("checkFillTwoFactor: Clicking submit button.");
+            sleep(3);
+            $this->exts->capture("2.2-two-factor-filled-" . $this->exts->two_factor_attempts);
+
+            $this->exts->moveToElementAndClick($two_factor_submit_selector);
+            sleep(10);
+            if ($this->exts->exists('div[class*="errorMessage"]')) {
+
+                $this->exts->capture("wrong 2FA code error-" . $this->exts->two_factor_attempts);
+                $this->exts->log('The code you entered is incorrect. Please try again.');
+            }
+
+            if ($this->exts->querySelector($two_factor_selector) == null) {
+                $this->exts->log("Two factor solved");
+            } else if ($this->exts->two_factor_attempts < 3) {
+                $this->exts->notification_uid = '';
+                $this->exts->two_factor_attempts++;
+                $this->checkFillTwoFactor();
+            } else {
+                $this->exts->log("Two factor can not solved");
+            }
+        } else {
+            $this->exts->log("Not received two factor code");
+        }
+    }
+}
