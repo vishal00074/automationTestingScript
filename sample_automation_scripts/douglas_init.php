5043c59d2651d832c582162e8ea87706467eb75a
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-08-01 18:26:21 +0530

    updated script

---
+++ sample_automation_scripts/douglas_init.php
@@ -0,0 +1,124 @@
+public $baseUrl = 'https://www.douglas.de/de/account';
+public $loginUrl = 'https://www.douglas.de/de/login';
+public $invoicePageUrl = 'https://www.douglas.de/de/account/orders';
+
+public $username_selector = 'form#loginForm input[name="email"]';
+public $password_selector = 'form#loginForm input[name="password"]';
+public $remember_me_selector = 'input[name="rememberMe"]';
+public $submit_login_selector = 'button.login__button[type="submit"]';
+
+public $check_login_failed_selector = 'div[data-testid="alert-error"]';
+public $check_login_success_selector = 'button[data-testid="signout-btn"]';
+
+public $isNoInvoice = true;
+
+/**
+
+    * Entry Method thats called for a portal
+
+    * @param Integer $count Number of times portal is retried.
+
+    */
+private function initPortal($count)
+{
+
+    $this->exts->log('Begin initPortal ' . $count);
+    $this->exts->loadCookiesFromFile();
+    $this->exts->openUrl($this->baseUrl);
+
+    if (!$this->checkLogin()) {
+        $this->exts->log('NOT logged via cookie');
+        $this->exts->clearCookies();
+        $this->exts->openUrl($this->loginUrl);
+        sleep(7);
+        $this->fillForm(0);
+    }
+
+    if ($this->checkLogin()) {
+        $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+        $this->exts->capture("LoginSuccess");
+
+        if (!empty($this->exts->config_array['allow_login_success_request'])) {
+            $this->exts->triggerLoginSuccess();
+        }
+
+        $this->exts->success();
+    } else {
+        if ($this->exts->exists($this->check_login_failed_selector)) {
+            $this->exts->log("Wrong credential !!!!");
+            $this->exts->loginFailure(1);
+        } else {
+            $this->exts->loginFailure();
+        }
+    }
+}
+
+public function fillForm($count)
+{
+    $this->exts->log("Begin fillForm " . $count);
+    $this->waitFor($this->username_selector, 10);
+    try {
+        if ($this->exts->querySelector($this->username_selector) != null) {
+
+            sleep(5);
+
+            $this->exts->capture("1-pre-login");
+            $this->exts->log("Enter Username");
+            $this->exts->moveToElementAndType($this->username_selector, $this->username);
+            sleep(1);
+
+            $this->exts->log("Enter Password");
+            $this->exts->moveToElementAndType($this->password_selector, $this->password);
+            sleep(1);
+
+            if ($this->exts->exists($this->remember_me_selector)) {
+                $this->exts->log("Remember Me");
+                $this->exts->moveToElementAndClick($this->remember_me_selector);
+                sleep(1);
+            }
+
+            $this->exts->capture("1-login-page-filled");
+            sleep(5);
+
+            if ($this->exts->exists($this->submit_login_selector)) {
+                $this->exts->click_by_xdotool($this->submit_login_selector);
+            }
+        }
+    } catch (\Exception $exception) {
+        $this->exts->log("Exception filling loginform " . $exception->getMessage());
+    }
+}
+
+public function waitFor($selector, $seconds = 7)
+{
+    for ($wait = 0; $wait < 2 && $this->exts->executeSafeScript("return !!document.querySelector('" . $selector . "');") != 1; $wait++) {
+        $this->exts->log('Waiting for Selectors.....');
+        sleep($seconds);
+    }
+}
+
+/**
+
+    * Method to Check where user is logged in or not
+
+    * return boolean true/false
+
+    */
+public function checkLogin()
+{
+    $this->exts->log("Begin checkLogin ");
+    $isLoggedIn = false;
+    try {
+        $this->waitFor($this->check_login_success_selector, 10);
+        if ($this->exts->exists($this->check_login_success_selector)) {
+
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+
+            $isLoggedIn = true;
+        }
+    } catch (Exception $exception) {
+        $this->exts->log("Exception checking loggedin " . $exception);
+    }
+
+    return $isLoggedIn;
+}
