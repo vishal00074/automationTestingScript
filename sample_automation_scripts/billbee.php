bf7772ad90f834d7f25e5a05fbd10303c462f4ea
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-08-14 18:02:43 +0530

    updated

--- sample_automation_scripts/billbee.php
+++ sample_automation_scripts/billbee.php
@@ -1,35 +1,42 @@
-<?php // remove waitTillPresent any because generating error and increase the sleep time after open base url
+<?php
 
 /**
  * Chrome Remote via Chrome devtool protocol script, for specific process/portal
  *
- * @package uwa
+ * @package	uwa
  *
- * @copyright   GetMyInvoices
+ * @copyright	GetMyInvoices
  */
 
-define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+define('KERNEL_ROOT', '/var/www/vhosts/worker/httpdocs/src/');
 
-$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
-require_once($gmi_browser_core);
+$gmi_selenium_core = realpath(KERNEL_ROOT . 'modules/cust_gmi_worker/includes/GmiChromeManager.php');
+require_once($gmi_selenium_core);
+
 class PortalScriptCDP
 {
 
-    private $exts;
-    public $setupSuccess = false;
+    private    $exts;
+    public    $setupSuccess = false;
     private $chrome_manage;
-    private $username;
-    private $password;
+    private    $username;
+    private    $password;
+    public $support_restart = true;
+    public $portal_domain = '';
 
     public function __construct($mode, $portal_name, $process_uid, $username, $password)
     {
-        $this->username = $username;
-        $this->password = $password;
+
+        $this->username = base64_decode($username);
+        $this->password = base64_decode($password);
 
         $this->exts = new GmiChromeManager();
-        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
-        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->exts->screen_capture_location = '/var/www/vhosts/worker/httpdocs/fs/cdo/process/2/2673482/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $this->username, $this->password);
         $this->setupSuccess = true;
+        if (!empty($this->exts->config_array['portal_domain'])) {
+            $this->portal_domain = $this->exts->config_array['portal_domain'];
+        }
     }
 
     /**
@@ -38,26 +45,31 @@ class PortalScriptCDP
     public function run()
     {
         if ($this->setupSuccess) {
+            file_put_contents($this->exts->screen_capture_location . '.script_execution_started', time());
             try {
+                // load cookies from file for desktop app
+                if (!empty($this->exts->config_array["without_password"])) {
+                    $this->exts->loadCookiesFromFile();
+                }
                 // Start portal script execution
                 $this->initPortal(0);
+
+                $this->exts->dump_session_files();
             } catch (\Exception $exception) {
-                $this->exts->log('Exception: ' . $exception->getMessage());
+                $this->exts->log('Selenium Exception: ' . $exception->getMessage());
                 $this->exts->capture("error");
                 var_dump($exception);
             }
 
-
             $this->exts->log('Execution completed');
 
             $this->exts->process_completed();
-            $this->exts->dump_session_files();
         } else {
             echo 'Script execution failed.. ' . "\n";
         }
     }
 
-    // Server-Portal-ID: 6097 - Last modified: 21.07.2025 14:05:50 UTC - User: 1
+    // Server-Portal-ID: 6097 - Last modified: 24.07.2025 13:24:12 UTC - User: 1
 
     public $baseUrl = 'https://app.billbee.io/app_v2/';
     public $loginUrl = 'https://app.billbee.io/app_v2/sign-in';
@@ -305,9 +317,5 @@ class PortalScriptCDP
     }
 }
 
-exec("docker rm -f selenium-node-111");
-exec("docker run -d --shm-size 2g -p 5902:5900 -p 9990:9999 -e TZ=Europe/Berlin -e SE_NODE_SESSION_TIMEOUT=86400 -e LANG=de -e GRID_TIMEOUT=0 -e GRID_BROWSER_TIMEOUT=0 -e SCREEN_WIDTH=1920 -e SCREEN_HEIGHT=1080 --name selenium-node-111 -v /var/www/remote-chrome/downloads:/home/seluser/Downloads/111 remote-chrome:v1");
-
-$browserSelected = 'chrome';
-$portal = new PortalScriptCDP($browserSelected, 'test_remote_chrome', '111', 'office@sayaq-adventures-muenchen.com', 'Sayaq#2022');
+$portal = new PortalScriptCDP("optimized-chrome-v2", 'Immowelt Kundenportal', '2673482', 'aW5mb0B0b20taW1tb2JpbGllbi5jb20=', 'UXQlb3FrV2VAKExSYjI=');
 $portal->run();
