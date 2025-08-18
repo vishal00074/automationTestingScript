2b41396e756a0861be1cdd3c1dd9d1dfe9209554
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-08-18 17:41:57 +0530

    heyflowaI replaced waitTillPresent with a custom JS waitFor function to prevent client read timeouts and handled empty invoice names. On the test engine, the second case failed due to the account_not_ready trigger for this connection.

---
+++ sample_automation_scripts/heyflow.php
@@ -0,0 +1,261 @@
+<?php // replace waitTillPresent to custom js waitFor function to prevent client read timout and handle empty invoices name
+
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package	uwa
+ *
+ * @copyright	GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/vhosts/worker/httpdocs/src/');
+
+$gmi_selenium_core = realpath(KERNEL_ROOT . 'modules/cust_gmi_worker/includes/GmiChromeManager.php');
+require_once($gmi_selenium_core);
+
+class PortalScriptCDP
+{
+
+    private    $exts;
+    public    $setupSuccess = false;
+    private $chrome_manage;
+    private    $username;
+    private    $password;
+    public $support_restart = true;
+    public $portal_domain = '';
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+
+        $this->username = base64_decode($username);
+        $this->password = base64_decode($password);
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/vhosts/worker/httpdocs/fs/cdo/process/2/2673482/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $this->username, $this->password);
+        $this->setupSuccess = true;
+        if (!empty($this->exts->config_array['portal_domain'])) {
+            $this->portal_domain = $this->exts->config_array['portal_domain'];
+        }
+    }
+
+    /**
+     * Method that called first for executing portal script, this method should not be altered by Users.
+     */
+    public function run()
+    {
+        if ($this->setupSuccess) {
+            file_put_contents($this->exts->screen_capture_location . '.script_execution_started', time());
+            try {
+                // load cookies from file for desktop app
+                if (!empty($this->exts->config_array["without_password"])) {
+                    $this->exts->loadCookiesFromFile();
+                }
+                // Start portal script execution
+                $this->initPortal(0);
+
+                $this->exts->dump_session_files();
+            } catch (\Exception $exception) {
+                $this->exts->log('Selenium Exception: ' . $exception->getMessage());
+                $this->exts->capture("error");
+                var_dump($exception);
+            }
+
+            $this->exts->log('Execution completed');
+
+            $this->exts->process_completed();
+        } else {
+            echo 'Script execution failed.. ' . "\n";
+        }
+    }
+
+    // Server-Portal-ID: 920850 - Last modified: 01.08.2025 13:39:07 UTC - User: 1
+
+    public $baseUrl = 'https://app.heyflow.com/login/';
+    public $loginUrl = 'https://app.heyflow.com/login/';
+    public $invoicePageUrl = 'https://app.heyflow.com/account/billing?from=/';
+
+    public $username_selector = 'input[type="email"]';
+    public $password_selector = 'input[type="password"]';
+    public $remember_me_selector = 'input#keeplogin';
+    public $submit_login_selector = 'button[type="submit"]';
+
+    public $check_login_failed_selector = 'div.input-container >div:nth-child(3)';
+    public $check_login_success_selector = 'div.avatar';
+
+    public $isNoInvoice = true;
+
+    /**
+
+     * Entry Method thats called for a portal
+
+     * @param Integer $count Number of times portal is retried.
+
+     */
+    private function initPortal($count)
+    {
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->loadCookiesFromFile();
+        $this->exts->openUrl($this->baseUrl);
+        sleep(10);
+        if (!$this->checkLogin()) {
+            $this->exts->log('NOT logged via cookie');
+            $this->fillForm(0);
+            sleep(10);
+        }
+
+        if ($this->checkLogin()) {
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+            $this->exts->capture("LoginSuccess");
+            $this->exts->openUrl($this->invoicePageUrl);
+            sleep(10);
+            $this->processInvoices();
+
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+            $this->exts->success();
+        } else {
+            if (stripos(strtolower($this->exts->extract($this->check_login_failed_selector)), 'password') !== false) {
+                $this->exts->log("Wrong credential !!!!");
+                $this->exts->loginFailure(1);
+            } elseif ($this->exts->urlContains('app.heyflow.com/checkout')) {
+                $this->exts->account_not_ready();
+            } else {
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    private function fillForm($count)
+    {
+        $this->exts->log("Begin fillForm " . $count);
+        try {
+            if ($this->exts->querySelector($this->username_selector) != null) {
+
+                $this->exts->capture("1-pre-login");
+                $this->exts->log("Enter Username");
+                $this->exts->moveToElementAndType($this->username_selector, $this->username);
+
+                $this->exts->log("Enter Password");
+                $this->exts->moveToElementAndType($this->password_selector, $this->password);
+                sleep(1);
+
+                if ($this->exts->exists($this->remember_me_selector)) {
+                    $this->exts->click_by_xdotool($this->remember_me_selector);
+                    sleep(1);
+                }
+
+                $this->exts->click_by_xdotool($this->submit_login_selector);
+                sleep(2); // Portal itself has one second delay after showing toast
+            }
+        } catch (\Exception $exception) {
+
+            $this->exts->log("Exception filling loginform " . $exception->getMessage());
+        }
+    }
+    
+
+    public function waitFor($selector, $seconds = 7)
+    {
+        for ($wait = 0; $wait < 2 && $this->exts->executeSafeScript("return !!document.querySelector('" . $selector . "');") != 1; $wait++) {
+            $this->exts->log('Waiting for Selectors.....');
+            sleep($seconds);
+        }
+    }
+
+
+    /**
+
+     * Method to Check where user is logged in or not
+
+     * return boolean true/false
+
+     */
+    private function checkLogin()
+    {
+        $this->exts->log("Begin checkLogin ");
+        $isLoggedIn = false;
+        try {
+            sleep(5);
+            $this->waitFor($this->check_login_success_selector, 10);
+            if ($this->isExists($this->check_login_success_selector)) {
+                $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+                $isLoggedIn = true;
+            }
+        } catch (Exception $exception) {
+
+            $this->exts->log("Exception checking loggedin " . $exception);
+        }
+        return $isLoggedIn;
+    }
+
+    private function isExists($selector = '')
+    {
+        $safeSelector = addslashes($selector);
+        $this->exts->log('Element:: ' . $safeSelector);
+        $isElement = $this->exts->execute_javascript('!!document.querySelector("' . $safeSelector . '")');
+        if ($isElement) {
+            $this->exts->log('Element Found');
+            return true;
+        } else {
+            $this->exts->log('Element not Found');
+            return false;
+        }
+    }
+
+    private function processInvoices($paging_count = 1)
+    {
+        $this->waitFor('table tbody tr a[href*="invoice"]', 10);
+        $this->exts->capture("4-invoices-page");
+        $invoices = [];
+
+        $rows = $this->exts->getElements("table tbody tr");
+        foreach ($rows as $row) {
+            if ($this->exts->querySelector('a[href*="invoice"]', $row) != null) {
+                $invoiceUrl = $this->exts->querySelector('a[href*="invoice"]', $row)->getAttribute('href');
+                $text = $this->exts->extract('td:nth-child(3)', $row);
+                preg_match('/- (.+)$/', $text, $matches);
+                $invoiceName = $this->exts->extract('td:nth-child(2)', $row);
+                $invoiceDate = trim($matches[1]);
+                $invoiceAmount = '';
+                $downloadBtn = $this->exts->querySelector('td:nth-child(5) a', $row);
+
+                array_push($invoices, array(
+                    'invoiceName' => $invoiceName,
+                    'invoiceDate' => $invoiceDate,
+                    'invoiceAmount' => $invoiceAmount,
+                    'invoiceUrl' => $invoiceUrl
+                ));
+                $this->isNoInvoice = false;
+            }
+        }
+
+        // Download all invoices
+        $this->exts->log('Invoices found: ' . count($invoices));
+        foreach ($invoices as $invoice) {
+            $this->exts->log('--------------------------');
+            $this->exts->log('invoiceName: ' . $invoice['invoiceName']);
+            $this->exts->log('invoiceDate: ' . $invoice['invoiceDate']);
+            $this->exts->log('invoiceAmount: ' . $invoice['invoiceAmount']);
+            $this->exts->log('invoiceUrl: ' . $invoice['invoiceUrl']);
+
+            $invoiceFileName = !empty($invoice['invoiceName']) ? $invoice['invoiceName'] . '.pdf': '';
+            $invoice['invoiceDate'] = $this->exts->parse_date($invoice['invoiceDate'], 'm.d.y', 'Y-m-d');
+            $this->exts->log('Date parsed: ' . $invoice['invoiceDate']);
+
+            $downloaded_file = $this->exts->direct_download($invoice['invoiceUrl'], 'pdf', $invoiceFileName);
+
+            if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                $this->exts->new_invoice($invoice['invoiceName'], $invoice['invoiceDate'], $invoice['invoiceAmount'], $invoiceFileName);
+                sleep(1);
+            } else {
+                $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+            }
+        }
+    }
+}
+
+$portal = new PortalScriptCDP("optimized-chrome-v2", 'Immowelt Kundenportal', '2673482', 'aW5mb0B0b20taW1tb2JpbGllbi5jb20=', 'UXQlb3FrV2VAKExSYjI=');
+$portal->run();
