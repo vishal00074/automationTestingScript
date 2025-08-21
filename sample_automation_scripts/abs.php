d41bc911cae9ac552d2dacad80cd3fb55457e61f
VISHAL CHAUDHARY <vc980531@gmail.com>
2025-08-21 18:12:39 +0530

    updated

---
+++ sample_automation_scripts/abs.php
@@ -0,0 +1,257 @@
+<?php //  added code to trigger no_invoice in case no invoices found added pagiation logic based on restrct page condition
+// and use click_and_download function to download the invoices  updated invoice rows selector
+/**
+ * Chrome Remote via Chrome devtool protocol script, for specific process/portal
+ *
+ * @package uwa
+ *
+ * @copyright   GetMyInvoices
+ */
+
+define('KERNEL_ROOT', '/var/www/remote-chrome/utils/');
+
+$gmi_browser_core = realpath('/var/www/remote-chrome/utils/GmiChromeManager.php');
+require_once($gmi_browser_core);
+class PortalScriptCDP
+{
+
+    private $exts;
+    public $setupSuccess = false;
+    private $chrome_manage;
+    private $username;
+    private $password;
+
+    public function __construct($mode, $portal_name, $process_uid, $username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->exts = new GmiChromeManager();
+        $this->exts->screen_capture_location = '/var/www/remote-chrome/screens/';
+        $this->exts->init($mode, $portal_name, $process_uid, $username, $password);
+        $this->setupSuccess = true;
+    }
+
+    /**
+     * Method that called first for executing portal script, this method should not be altered by Users.
+     */
+    public function run()
+    {
+        if ($this->setupSuccess) {
+            try {
+                // Start portal script execution
+                $this->initPortal(0);
+            } catch (\Exception $exception) {
+                $this->exts->log('Exception: ' . $exception->getMessage());
+                $this->exts->capture("error");
+                var_dump($exception);
+            }
+
+
+            $this->exts->log('Execution completed');
+
+            $this->exts->process_completed();
+            $this->exts->dump_session_files();
+        } else {
+            echo 'Script execution failed.. ' . "\n";
+        }
+    }
+
+    // Server-Portal-ID: 1040952 - Last modified: 14.02.2025 05:54:22 UTC - User: 1
+
+    /*Define constants used in script*/
+
+    public $baseUrl = "https://portal.abs-rz.de/";
+    public $loginUrl = "https://portal.abs-rz.de/";
+    public $invoicePageUrl = '';
+    public $username_selector = 'input[id="login_login"]';
+    public $password_selector = 'input[id="login_passwd"]';
+    public $submit_button_selector = 'button[name="login"]';
+    public $check_login_failed_selector = 'div[id="message_error"] > div > div > label';
+    public $check_login_success_selector = 'h1[name="page-headline"]';
+    public $login_tryout = 0;
+    public $isNoInvoice = true;
+    public $restrictPages = 3;
+    /**
+
+     * Entry Method thats called for a portal
+
+     * @param Integer $count Number of times portal is retried.
+
+     */
+
+    private function initPortal($count)
+    {
+        $this->restrictPages = isset($this->exts->config_array["restrictPages"]) ? (int) @$this->exts->config_array["restrictPages"] : 3;
+        $this->exts->log('Begin initPortal ' . $count);
+        $this->exts->loadCookiesFromFile();
+        $this->exts->openUrl($this->baseUrl);
+
+        if (!$this->checkLogin()) {
+            $this->exts->log('NOT logged via cookie');
+            $this->exts->clearCookies();
+            $this->exts->openUrl($this->loginUrl);
+            $this->fillForm(0);
+        }
+        if ($this->checkLogin()) {
+            $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+            $this->exts->capture("LoginSuccess");
+
+            sleep(5);
+            $this->exts->click_element('span[data-bs-target="#top_menu"]');
+            sleep(5);
+            $this->exts->click_element('ul[name="user-menu-items"] > li:nth-child(3) > a');
+            sleep(5);
+            $this->processInvoices();
+
+            // Final, check no invoice
+            if ($this->isNoInvoice) {
+                $this->exts->no_invoice();
+            }
+            $this->exts->success();
+        } else {
+            $this->exts->waitTillPresent($this->check_login_failed_selector, 10);
+            if ($this->exts->exists($this->check_login_failed_selector)) {
+                $this->exts->log("Wrong credential !!!!");
+                $this->exts->loginFailure(1);
+            } else {
+                $this->exts->log("Failed due to unknown reasons");
+                $this->exts->loginFailure();
+            }
+        }
+    }
+
+    function fillForm($count)
+    {
+        $this->exts->log("Begin fillForm " . $count);
+        $this->exts->waitTillPresent($this->username_selector, 20);
+        try {
+            if ($this->exts->querySelector($this->username_selector) != null) {
+
+                $this->login_tryout = (int)$this->login_tryout + 1;
+                $this->exts->capture("1-pre-login");
+                $this->exts->log("Enter Username");
+                $this->exts->moveToElementAndType($this->username_selector, $this->username);
+
+                $this->exts->log("Enter Password");
+                $this->exts->moveToElementAndType($this->password_selector, $this->password);
+                sleep(1);
+
+                $this->exts->click_by_xdotool($this->submit_button_selector);
+                sleep(2); // Portal itself has one second delay after showing toast
+            }
+        } catch (\Exception $exception) {
+
+            $this->exts->log("Exception filling loginform " . $exception->getMessage());
+        }
+    }
+    /**
+
+     * Method to Check where user is logged in or not
+
+     * return boolean true/false
+
+     */
+    function checkLogin()
+    {
+        $this->exts->log("Begin checkLogin ");
+        $isLoggedIn = false;
+        try {
+            $this->exts->waitTillPresent($this->check_login_success_selector, 20);
+            sleep(10);
+            if ($this->exts->querySelector($this->check_login_success_selector) && $this->exts->querySelector($this->check_login_success_selector)->getText() != '') {
+
+                $this->exts->log(">>>>>>>>>>>>>>>Login successful!!!!");
+
+                $isLoggedIn = true;
+            }
+        } catch (Exception $exception) {
+
+            $this->exts->log("Exception checking loggedin " . $exception);
+        }
+
+        return $isLoggedIn;
+    }
+
+    private function processInvoices($count = 0)
+    {
+        $this->exts->capture($count."-invoice page");
+        $invoices = [];
+        $rows = $this->exts->querySelectorAll(' table[id="invoices_table"] > tbody > tr[style=""]');
+        foreach ($rows as $row) {
+
+            $invoiceName = $row->querySelector('td:nth-child(1) > div');
+            if ($invoiceName  != null) {
+                $invoiceName = $invoiceName->getText();
+            }
+
+            $invoiceDate = $row->querySelector('td:nth-child(2) > div');
+            if ($invoiceDate  != null) {
+                $invoiceDate = $invoiceDate->getText();
+            }
+
+            $amount = $row->querySelector('td:nth-child(3) > div');
+            if ($amount  != null) {
+                $amount = $amount->getText();
+            }
+
+            $downloadLink = $row->querySelector('td:nth-child(1) > div');
+
+            array_push($invoices, array(
+                'invoiceName' => $invoiceName,
+                'invoiceDate' => $invoiceDate,
+                'invoiceAmount' => $amount,
+                'downloadLink' => $downloadLink,
+            ));
+
+            $this->isNoInvoice = false;
+        }
+        $this->exts->log('Invoices found: ' . count($invoices));
+        foreach ($invoices as $invoice) {
+            if ($invoice['downloadLink'] != null) {
+                $this->exts->log('--------------------------');
+                $this->exts->log('invoiceName: ' . $invoice['invoiceName']);
+                $this->exts->log('invoiceDate: ' . $invoice['invoiceDate']);
+                $this->exts->log('invoiceDate: ' . $invoice['invoiceAmount']);
+
+                $invoiceFileName = !empty($invoice['invoiceName']) ? $invoice['invoiceName'] . '.pdf' : '';
+                $this->exts->click_element($invoice['downloadLink']);
+                sleep(5);
+                $downloaded_file = $this->exts->click_and_download('a[name="pdf-dl"]', 'pdf', $invoiceFileName);
+                sleep(4);
+                if (trim($downloaded_file) != '' && file_exists($downloaded_file)) {
+                    $this->exts->new_invoice($invoice['invoiceName'], $invoice['invoiceDate'], $invoice['invoiceAmount'], $invoiceFileName);
+                    sleep(1);
+                } else {
+                    $this->exts->log(__FUNCTION__ . '::No download ' . $invoiceFileName);
+                }
+                $this->exts->click_element('div[id="pdfModal"] > div > div > div > button');
+                sleep(5);
+            }
+        }
+
+        $pagiantionSelector = 'table#invoices_table ul.pagination > li[class="page-item next"]:not(.disabled) button';
+        if ($this->restrictPages == 0) {
+            if ($count < 50 && $this->exts->querySelector($pagiantionSelector) != null) {
+                $this->exts->click_by_xdotool($pagiantionSelector);
+                sleep(7);
+                $count++;
+                $this->processInvoices($count);
+            }
+        } else {
+            if ($count < $this->restrictPages && $this->exts->querySelector($pagiantionSelector) != null) {
+                $this->exts->click_by_xdotool($pagiantionSelector);
+                sleep(7);
+                $count++;
+                $this->processInvoices($count);
+            }
+        }
+    }
+}
+
+exec("docker rm -f selenium-node-111");
+exec("docker run -d --shm-size 2g -p 5902:5900 -p 9990:9999 -e TZ=Europe/Berlin -e SE_NODE_SESSION_TIMEOUT=86400 -e LANG=de -e GRID_TIMEOUT=0 -e GRID_BROWSER_TIMEOUT=0 -e SCREEN_WIDTH=1920 -e SCREEN_HEIGHT=1080 --name selenium-node-111 -v /var/www/remote-chrome/downloads:/home/seluser/Downloads/111 remote-chrome:v1");
+
+$browserSelected = 'chrome';
+$portal = new PortalScriptCDP($browserSelected, 'test_remote_chrome', '111', 'office@sayaq-adventures-muenchen.com', 'Sayaq#2022');
+$portal->run();
